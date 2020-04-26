08f5aae16330f8f8a70ed10427472f3712ddeeaa
Adam Dean <adam.dean@chem.ox.ac.uk>
2020-04-26 12:59:39 +0100

    Added *.dat to gitignore and sped up writing populations

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import people
 
 if __name__ == '__main__':
 
-    years = 1000
+    years = 5
 
-    dict_num = { 'null'      : 10000,
+    dict_num = { 'null'      : 100000,
                  'water'     : 0,
                  'fire'      : 0,
                  'earth'     : 0,
@@ -79,42 +79,39 @@ if __name__ == '__main__':
                 if num > 0:
                     f.write(soul + ' = ' + str(num) + '\n')
 
+        start = time.time()
+        sls = [person.soul for person in persons]
         with open('population.dat', 'a') as f:
-            f.write(str(year) + ' ' + str(len(persons)) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'null'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'water'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'fire'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'earth'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'wind'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'light'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'dark'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'stone'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'metal'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'flying'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'ice'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'lightning'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'poison'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'ghost'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'psychic'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'nuclear'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'gravity'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'life'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'death'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'soul'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'luna'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'jade'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'dragon'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'dream'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'blood'])) +\
-                                ' ' + str(len([True for person in persons if person.soul == 'arcane'])) +\
+            f.write(str(year) + ' ' + str(len(sls)) +\
+                                ' ' + str(sls.count('null')) +\
+                                ' ' + str(sls.count('water')) +\
+                                ' ' + str(sls.count('fire')) +\
+                                ' ' + str(sls.count('earth')) +\
+                                ' ' + str(sls.count('wind')) +\
+                                ' ' + str(sls.count('light')) +\
+                                ' ' + str(sls.count('dark')) +\
+                                ' ' + str(sls.count('stone')) +\
+                                ' ' + str(sls.count('metal')) +\
+                                ' ' + str(sls.count('flying')) +\
+                                ' ' + str(sls.count('ice')) +\
+                                ' ' + str(sls.count('lightning')) +\
+                                ' ' + str(sls.count('poison')) +\
+                                ' ' + str(sls.count('ghost')) +\
+                                ' ' + str(sls.count('psychic')) +\
+                                ' ' + str(sls.count('nuclear')) +\
+                                ' ' + str(sls.count('gravity')) +\
+                                ' ' + str(sls.count('life')) +\
+                                ' ' + str(sls.count('death')) +\
+                                ' ' + str(sls.count('soul')) +\
+                                ' ' + str(sls.count('luna')) +\
+                                ' ' + str(sls.count('jade')) +\
+                                ' ' + str(sls.count('dragon')) +\
+                                ' ' + str(sls.count('dream')) +\
+                                ' ' + str(sls.count('blood')) +\
+                                ' ' + str(sls.count('arcane')) +\
                     '\n')
-
-    #for person in persons:
-    #    print(person.soul, 'alive=' + str(person.alive) + (str(person.year_of_death) if not person.alive else ''), 'sex=' + person.sex, 'age=' + str(person.age), 'partner=' + str(True if person.partner else False), 'children=' + str(person.num_children))
-
-    #for soul in people.dict_souls:
-    #    print(soul + ' = ' + str(len([True for person in persons if person.soul == soul])))
-
+        end = time.time()
+        #print('time' + str(end-start))
 
     with open('individuals.dat', 'a') as f:
         for person in persons:
